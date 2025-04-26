72e20bd805c66054a0916935458b86823f5d7619
Yassine El Anssari <yassinedvp25@gmail.com>
2025-04-26 11:55:52 +0100

    added splash brand night mode

---
+++ advancecourse/lib/core/helpers/spacing.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+SizedBox verticalSpace(double height) {
+  return SizedBox(height: height.h);
+}
+SizedBox horizanralSpace(double height) {
+  return SizedBox(height: height.h);
+}
